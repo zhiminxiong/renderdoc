2d80b42223651a5ee6db2d424399e2be67bb8b35
alexxiong <alexxiong@tencent.com>
2026-01-25 09:29:10 +0800

    feat: flat模式下拉列表增加hover状态

--- qrenderdoc/Windows/EventBrowser.cpp
+++ qrenderdoc/Windows/EventBrowser.cpp
@@ -4030,6 +4030,13 @@ EventBrowser::EventBrowser(ICaptureContext &ctx, QWidget *parent)
   // Connect view mode combo box
   QObject::connect(ui->viewMode, OverloadedSlot<int>::of(&QComboBox::currentIndexChanged), this,
                    &EventBrowser::on_viewMode_currentIndexChanged);
+  
+  // Enable hover effect for the dropdown list
+  QListView *viewModeView = qobject_cast<QListView *>(ui->viewMode->view());
+  if(viewModeView)
+  {
+    viewModeView->setMouseTracking(true);
+  }
 
   {
     QMenu *extensionsMenu = new QMenu(this);
